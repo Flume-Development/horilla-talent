c3571071f71b9fb41d4ef58d9f64303f2c8bd58b
Omondi <omondi@flume.co.za>
2026-01-23 15:08:31 +0200

    Remove ui

--- horilla-frontend/layouts/blank.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-950">
-    <slot />
-  </div>
-</template>
-
-<script setup lang="ts">
-// Blank layout - no sidebar or navigation
-</script>
